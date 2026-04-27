cf294a2b6438c50223aac364edfc723570b44a18
Christian <christianjfr@gmail.com>
2026-04-27 15:24:25 -0500

    fix: proyect name for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 
-const production = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
 	root: 'src',
-	base: production ? 'production' : 'development',
+	base: isProduction ? '/vault/' : 'development',
 	publicDir: '../public',
 	plugins: [],
 	server: { port: 5173 },
